0fed7322e85358b8c7addc77281b3069608d9b63
Ryan Harrison <mail@ryanharrison.co.uk>
2018-03-02 18:25:34 +0000

    Add HikariCP connection pooling

--- src/main/kotlin/service/DatabaseFactory.kt
+++ src/main/kotlin/service/DatabaseFactory.kt
@@ -1,5 +1,7 @@
 package service
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import kotlinx.coroutines.experimental.newFixedThreadPoolContext
 import kotlinx.coroutines.experimental.withContext
 import model.*
@@ -12,7 +14,8 @@ import kotlin.coroutines.experimental.CoroutineContext
 object DatabaseFactory {
 
     fun init() {
-        Database.connect("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", driver = "org.h2.Driver")
+        // Database.connect("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", driver = "org.h2.Driver")
+        Database.connect(hikari())
         transaction {
             create(Widgets)
             Widgets.insert {
@@ -28,6 +31,16 @@ object DatabaseFactory {
         }
     }
 
+    private fun hikari(): HikariDataSource {
+        val config = HikariConfig()
+        config.driverClassName = "org.h2.Driver"
+        config.jdbcUrl = "jdbc:h2:mem:test"
+        config.maximumPoolSize = 3
+        config.isAutoCommit = false
+        config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        config.validate()
+        return HikariDataSource(config)
+    }
 
     private val dispatcher: CoroutineContext
 
