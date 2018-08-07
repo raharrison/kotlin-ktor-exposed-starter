5bc0dfef97de035f7fb1c5a16801f0f6b1e8f0d8
Ryan Harrison <mail@ryanharrison.co.uk>
2018-08-07 20:34:37 +0100

    Add tests for widget service

---
+++ src/test/kotlin/common/ServerTest.kt
@@ -0,0 +1,55 @@
+package common
+
+import io.ktor.application.Application
+import io.ktor.server.engine.ApplicationEngine
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.restassured.RestAssured
+import io.restassured.response.ResponseBodyExtractionOptions
+import io.restassured.specification.RequestSpecification
+import model.Widgets
+import module
+import org.jetbrains.exposed.sql.deleteAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import java.util.concurrent.TimeUnit
+
+open class ServerTest {
+
+    protected fun RequestSpecification.When(): RequestSpecification {
+        return this.`when`()
+    }
+
+    protected inline fun <reified T> ResponseBodyExtractionOptions.to(): T {
+        return this.`as`(T::class.java)
+    }
+
+    companion object {
+
+        private var serverStarted = false
+
+        private lateinit var server: ApplicationEngine
+
+        @BeforeAll
+        @JvmStatic
+        fun startServer() {
+            if(!serverStarted) {
+                server = embeddedServer(Netty, 8080, watchPaths = listOf("Main"), module = Application::module)
+                server.start()
+                serverStarted = true
+
+                RestAssured.baseURI = "http://localhost"
+                RestAssured.port = 8080
+                Runtime.getRuntime().addShutdownHook(Thread { server.stop(0, 0, TimeUnit.SECONDS) })
+            }
+        }
+    }
+
+    @BeforeEach
+    fun before() = transaction {
+        Widgets.deleteAll()
+        Unit
+    }
+
+}
---
+++ src/test/kotlin/service/WidgetServiceTest.kt
@@ -0,0 +1,105 @@
+package service
+
+import common.ServerTest
+import kotlinx.coroutines.experimental.runBlocking
+import model.NewWidget
+import model.Widget
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class WidgetServiceTest: ServerTest() {
+
+    private val widgetService = WidgetService()
+
+    @Test
+    fun testAddWidget() = runBlocking {
+        // when
+        val widget1 = NewWidget(null, "widget1", 10)
+
+        // then
+        val saved = addWidget(widget1)
+
+        // verify
+        val retrieved = widgetService.getWidget(saved.id)
+        assertThat(retrieved).isEqualTo(saved)
+        assertThat(retrieved?.name).isEqualTo(widget1.name)
+        assertThat(retrieved?.quantity).isEqualTo(widget1.quantity)
+
+        Unit
+    }
+
+    @Test
+    fun testGetAllWidgets() = runBlocking {
+        // when
+        val widget1 = NewWidget(null, "widget1", 10)
+        val widget2 = NewWidget(null, "widget2", 5)
+        addWidget(widget1)
+        addWidget(widget2)
+
+        // then
+        val widgets = widgetService.getAllWidgets()
+
+        assertThat(widgets).hasSize(2)
+        assertThat(widgets).extracting("name").containsExactlyInAnyOrder(widget1.name, widget2.name)
+        assertThat(widgets).extracting("quantity").containsExactlyInAnyOrder(widget1.quantity, widget2.quantity)
+
+        Unit
+    }
+
+    @Test
+    fun testUpdateWidget() = runBlocking {
+        // when
+        val widget1 = NewWidget(null, "widget1", 10)
+        val saved = addWidget(widget1)
+
+        // then
+        val update = NewWidget(saved.id, "updated", 46)
+        val updated = widgetService.updateWidget(update)
+
+        assertThat(updated).isNotNull
+        assertThat(updated?.id).isEqualTo(update.id)
+        assertThat(updated?.name).isEqualTo(update.name)
+        assertThat(updated?.quantity).isEqualTo(update.quantity)
+
+        assertThat(widgetService.getWidget(saved.id)).isEqualTo(updated)
+
+        Unit
+    }
+
+    @Test
+    fun testUpdateWidgetNoIdInserts() = runBlocking {
+        // when
+        val widget1 = NewWidget(null, "widget1", 10)
+        val inserted = widgetService.updateWidget(widget1)
+
+        assertThat(inserted).isNotNull
+
+        val retrieved = widgetService.getWidget(inserted?.id!!)
+        assertThat(retrieved?.name).isEqualTo(widget1.name)
+        assertThat(retrieved?.quantity).isEqualTo(widget1.quantity)
+
+        Unit
+    }
+
+    @Test
+    fun testDeleteWidget() = runBlocking {
+        // when
+        val widget1 = NewWidget(null, "widget1", 10)
+        val saved = addWidget(widget1)
+
+        // then
+        assertThat(widgetService.getWidget(saved.id)).isNotNull
+        val result = widgetService.deleteWidget(saved.id)
+
+        // verify
+        assertThat(result).isTrue()
+        assertThat(widgetService.getWidget(saved.id)).isNull()
+        assertThat(widgetService.getAllWidgets()).isEmpty()
+        Unit
+    }
+
+    private suspend fun addWidget(widget: NewWidget): Widget {
+        return widgetService.addWidget(widget)
+    }
+}
