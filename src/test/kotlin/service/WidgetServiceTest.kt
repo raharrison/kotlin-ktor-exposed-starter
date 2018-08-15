3649c66ac26845a3904af65f9d8f5d53b7cc588c
Ryan Harrison <mail@ryanharrison.co.uk>
2018-08-15 20:57:35 +0100

    Add notification tests fixes #3

--- src/test/kotlin/service/WidgetServiceTest.kt
+++ src/test/kotlin/service/WidgetServiceTest.kt
@@ -2,7 +2,9 @@ package service
 
 import common.ServerTest
 import kotlinx.coroutines.experimental.runBlocking
+import model.ChangeType
 import model.NewWidget
+import model.Notification
 import model.Widget
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
@@ -14,13 +16,13 @@ class WidgetServiceTest: ServerTest() {
 
     @Test
     fun testAddWidget() = runBlocking {
-        // when
+        // given
         val widget1 = NewWidget(null, "widget1", 10)
 
-        // then
+        // when
         val saved = addWidget(widget1)
 
-        // verify
+        // then
         val retrieved = widgetService.getWidget(saved.id)
         assertThat(retrieved).isEqualTo(saved)
         assertThat(retrieved?.name).isEqualTo(widget1.name)
@@ -31,15 +33,16 @@ class WidgetServiceTest: ServerTest() {
 
     @Test
     fun testGetAllWidgets() = runBlocking {
-        // when
+        // given
         val widget1 = NewWidget(null, "widget1", 10)
         val widget2 = NewWidget(null, "widget2", 5)
         addWidget(widget1)
         addWidget(widget2)
 
-        // then
+        // when
         val widgets = widgetService.getAllWidgets()
 
+        // then
         assertThat(widgets).hasSize(2)
         assertThat(widgets).extracting("name").containsExactlyInAnyOrder(widget1.name, widget2.name)
         assertThat(widgets).extracting("quantity").containsExactlyInAnyOrder(widget1.quantity, widget2.quantity)
@@ -49,14 +52,15 @@ class WidgetServiceTest: ServerTest() {
 
     @Test
     fun testUpdateWidget() = runBlocking {
-        // when
+        // given
         val widget1 = NewWidget(null, "widget1", 10)
         val saved = addWidget(widget1)
 
-        // then
+        // when
         val update = NewWidget(saved.id, "updated", 46)
         val updated = widgetService.updateWidget(update)
 
+        // then
         assertThat(updated).isNotNull
         assertThat(updated?.id).isEqualTo(update.id)
         assertThat(updated?.name).isEqualTo(update.name)
@@ -69,10 +73,11 @@ class WidgetServiceTest: ServerTest() {
 
     @Test
     fun testUpdateWidgetNoIdInserts() = runBlocking {
-        // when
+        // given
         val widget1 = NewWidget(null, "widget1", 10)
         val inserted = widgetService.updateWidget(widget1)
 
+        // then
         assertThat(inserted).isNotNull
 
         val retrieved = widgetService.getWidget(inserted?.id!!)
@@ -84,19 +89,96 @@ class WidgetServiceTest: ServerTest() {
 
     @Test
     fun testDeleteWidget() = runBlocking {
-        // when
+        // given
         val widget1 = NewWidget(null, "widget1", 10)
         val saved = addWidget(widget1)
 
-        // then
+        // when
         assertThat(widgetService.getWidget(saved.id)).isNotNull
         val result = widgetService.deleteWidget(saved.id)
 
-        // verify
+        // then
         assertThat(result).isTrue()
         assertThat(widgetService.getWidget(saved.id)).isNull()
         assertThat(widgetService.getAllWidgets()).isEmpty()
         Unit
+    }
+
+    @Nested
+    inner class NotificationCases {
+
+        @Test
+        fun testNotifyAdd() = runBlocking {
+            val widget1 = NewWidget(null, "widget1", 10)
+
+            var called = false
+            val func: suspend (Notification<Widget?>) -> Unit = {
+                assertThat(it.type).isEqualTo(ChangeType.CREATE)
+                assertThat(it.entity?.name).isEqualTo(widget1.name)
+                assertThat(it.entity?.quantity).isEqualTo(widget1.quantity)
+                called = true
+            }
+
+            widgetService.addChangeListener(123, func)
+            addWidget(widget1)
+            assertThat(called).isTrue()
+            Unit
+        }
+
+        @Test
+        fun testNotifyUpdate() = runBlocking {
+            val widget1 = NewWidget(null, "widget1", 10)
+            val saved = addWidget(widget1)
+            val updated = NewWidget(null, "updated", 25)
+
+            var called = false
+            val func: suspend (Notification<Widget?>) -> Unit = {
+                assertThat(it.type).isEqualTo(ChangeType.UPDATE)
+                assertThat(it.entity?.name).isEqualTo(updated.name)
+                assertThat(it.entity?.quantity).isEqualTo(updated.quantity)
+                assertThat(it.id).isEqualTo(saved.id)
+                called = true
+            }
+
+            widgetService.addChangeListener(123, func)
+            widgetService.updateWidget(updated.copy(id=saved.id))
+            assertThat(called).isTrue()
+            Unit
+        }
+
+        @Test
+        fun testNotifyDelete() = runBlocking {
+            val widget1 = NewWidget(null, "widget1", 10)
+            val saved = addWidget(widget1)
+
+            var called = false
+            val func: suspend (Notification<Widget?>) -> Unit = {
+                assertThat(it.type).isEqualTo(ChangeType.DELETE)
+                assertThat(it.entity).isNull()
+                assertThat(it.id).isEqualTo(saved.id)
+                called = true
+            }
+
+            widgetService.addChangeListener(123, func)
+            widgetService.deleteWidget(saved.id)
+            assertThat(called).isTrue()
+            Unit
+        }
+
+        @Test
+        fun testRemoveListener() = runBlocking {
+            var called = false
+            val func: suspend (Notification<Widget?>) -> Unit = {
+                called = true
+            }
+
+            widgetService.addChangeListener(123, func)
+            widgetService.removeChangeListener(123)
+            addWidget(NewWidget(null, "widget1", 10))
+            assertThat(called).isFalse()
+            Unit
+        }
+
     }
 
     @Nested
