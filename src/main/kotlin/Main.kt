2d54dd4f8c6bc5119680c7ea316149290e4c3b28
Ryan Harrison <mail@ryanharrison.co.uk>
2019-08-10 20:10:01 +0100

    Use config from application.conf fixes #6

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,7 @@ import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
 import io.ktor.jackson.jackson
 import io.ktor.routing.Routing
+import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.websocket.WebSockets
@@ -34,6 +35,6 @@ fun Application.module() {
 
 }
 
-fun main() {
-    embeddedServer(Netty, 8080, watchPaths = listOf("MainKt"), module = Application::module).start()
+fun main(args: Array<String>) {
+    embeddedServer(Netty, commandLineEnvironment(args)).start(wait = true)
 }
