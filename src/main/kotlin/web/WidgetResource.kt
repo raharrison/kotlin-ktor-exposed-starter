0ca01ffbf82376a4d697edf15945bd055f7ef9bf
Ryan Harrison <mail@ryanharrison.co.uk>
2021-04-10 12:11:31 +0100

    Update routing

--- src/main/kotlin/web/WidgetResource.kt
+++ src/main/kotlin/web/WidgetResource.kt
@@ -20,7 +20,7 @@ fun Route.widget(widgetService: WidgetService) {
 
     route("/widget") {
 
-        get("/") {
+        get {
             call.respond(widgetService.getAllWidgets())
         }
 
@@ -31,12 +31,12 @@ fun Route.widget(widgetService: WidgetService) {
             else call.respond(widget)
         }
 
-        post("/") {
+        post {
             val widget = call.receive<NewWidget>()
             call.respond(HttpStatusCode.Created, widgetService.addWidget(widget))
         }
 
-        put("/") {
+        put {
             val widget = call.receive<NewWidget>()
             val updated = widgetService.updateWidget(widget)
             if (updated == null) call.respond(HttpStatusCode.NotFound)
@@ -44,7 +44,7 @@ fun Route.widget(widgetService: WidgetService) {
         }
 
         delete("/{id}") {
-            val id = call.parameters["id"]?.toInt() ?: throw IllegalStateException("Must provide id");
+            val id = call.parameters["id"]?.toInt() ?: throw IllegalStateException("Must provide id")
             val removed = widgetService.deleteWidget(id)
             if (removed) call.respond(HttpStatusCode.OK)
             else call.respond(HttpStatusCode.NotFound)
