10276d068de57cbaf4fa50bdd4012b169f117fdf
Ryan Harrison <mail@ryanharrison.co.uk>
2021-04-10 12:14:05 +0100

    Update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val ktorVersion = "1.5.0"
-val exposedVersion = "0.29.1"
+val ktorVersion = "1.5.3"
+val exposedVersion = "0.30.1"
 val h2Version = "1.4.200"
 val hikariCpVersion = "4.0.2"
-val flywayVersion = "7.5.3"
+val flywayVersion = "7.7.3"
 val ktorFlywayVersion = "1.2.2"
 val logbackVersion = "1.2.3"
 val assertjVersion = "3.19.0"
@@ -12,7 +12,7 @@ val restAssuredVersion = "4.3.3"
 val junitVersion = "5.7.1"
 
 plugins {
-    kotlin("jvm") version "1.4.30"
+    kotlin("jvm") version "1.4.32"
     application
 }
 
