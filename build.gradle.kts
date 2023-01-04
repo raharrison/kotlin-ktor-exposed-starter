99cabb1a3c76923a359ac107f4309796d6d74f6a
Ryan Harrison <mail@ryanharrison.co.uk>
2023-01-04 18:43:50 +0000

    Update to Kotlin 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
-val ktorVersion = "2.2.1"
+val ktorVersion = "2.2.2"
 val exposedVersion = "0.41.1"
 val h2Version = "2.1.214"
 val hikariCpVersion = "5.0.1"
-val flywayVersion = "9.8.3"
+val flywayVersion = "9.10.2"
 val logbackVersion = "1.4.5"
 val assertjVersion = "3.23.1"
 val restAssuredVersion = "5.3.0"
 val junitVersion = "5.9.1"
 
 plugins {
-    kotlin("jvm") version "1.7.20"
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("jvm") version "1.8.0"
+    kotlin("plugin.serialization") version "1.8.0"
     id("org.jetbrains.kotlinx.kover") version "0.6.1"
     application
 }
